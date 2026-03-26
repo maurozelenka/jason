bb2a862611b723686cd15b2f65dea07727a1f148
xMaurox10 <zelenkamauro@gmail.com>
2026-03-26 19:05:16 +0100

    feat: add VS Code extension recommendations to new projects (JaCaMo)

--- jason-cli/src/main/java/jason/cli/app/Create.java
+++ jason-cli/src/main/java/jason/cli/app/Create.java
@@ -85,6 +85,7 @@ public class Create implements Runnable {
         copyFile(masName, "vscode-tasks.json",  "",      new File( path + "/.vscode/tasks.json"), console);
         copyFile(masName, "vscode-launch.json", "",      new File( path + "/.vscode/launch.json"), console);
         copyFile(masName, "vscode-settings.json", "",    new File( path + "/.vscode/settings.json"), console);
+        copyFile(masName, "vscode-extensions.json", "",  new File( path + "/.vscode/extensions.json"), console);
     }
 
     public static void copyFile(String id, String source, String agName, File target, boolean consoleApp) {
